8c8d631bac46825dd45a72b7100a41a1d0ea6020
danilo <daanilo-silvaa@hotmail.com>
2021-08-27 10:33:57 -0300

    componentState vue

---
+++ resources/js/components/States/States.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <select name="state" id="">
+            <option value="">Selecione o seu Estado</option>
+        </select>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
